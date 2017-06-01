6ae2c47103a90f84f3b6d577d415b72ad4090347
Michael Kant <contact@michaelkant.com>
2017-05-31 20:56:57 -0700

    sort events by date chronological

--- client/views/locations/locations.js
+++ client/views/locations/locations.js
@@ -6,12 +6,12 @@ Template.locations.helpers({
 	permEvents: function() {
 		return Events.find({
 			duration: { $in: ['perm'] }
-		});
+		}, {sort: { date: 1 }});
 	},
 	tempEvents: function() {
 		return Events.find({
 			duration: { $in: ['temp'] }
-		});
+		}, {sort: { date: 1 }});
 	},
 
 	formatDate: function() {
