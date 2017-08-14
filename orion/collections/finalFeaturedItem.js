1ec6e33107a2614306c0dd2dcc57e87a292cd9eb
Michael Kant <contact@michaelkant.com>
2017-08-13 19:18:01 -0700

    change the size of featured items

--- orion/collections/finalFeaturedItem.js
+++ orion/collections/finalFeaturedItem.js
@@ -1,13 +1,13 @@
 
 FinalFeaturedItem = new orion.collection('finalFeaturedItem', {
-    singularName: 'Final Featured Item', // The name of one of this items
-    pluralName: 'Final Featured Items', // The name of more than one of this items
-    title: 'Final Featured Item', // The title of the page
+    singularName: 'Featured Item', // The name of one of this items
+    pluralName: 'Featured Items', // The name of more than one of this items
+    title: 'Featured Item', // The title of the page
     link: {
         /**
         * The text that you want to show in the sidebar. The default value is the name of the collection, so in this case is not necesary
         */
-        title: 'Final Featured Items'
+        title: 'Featured Items'
     },
     /**
     * Tabular settings for this collection
